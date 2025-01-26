636fe8df94c84c4af07bf605e4b24275ba0fdda8
Robert Bledsaw III <rbledsaw3@outlook.com>
2025-01-26 17:21:18 -0500

    feat: implement thumbnail upload handler
    - Add multipart form parsing for image uploads
    - Store thumbnails in memory using global videoThumbnails map
    - Update video records with thumbnail URLs
    - Implement authentication checks for video ownership
    - Handle proper content type and data storage

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
@@ -28,10 +29,70 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse thumbnail", err)
+		return
+	}
+	defer file.Close()
+
+	// get media type from the file's `Content-Type` header
+	contentType := header.Header.Get("Content-Type")
+
+	// read all image data into byte slice using `io.ReadAll`
+	thumbnailData, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't read thumbnail", err)
+		return
+	}
+
+	// get video's metadata from SQLite database. `apiConfig.db` has a `GetVideo` method
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get video", err)
+		return
+	}
+	// if authenticated user is not the video owner, return `http.StatusUnauthorized` response
+	if video.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "Unauthorized", nil)
+		return
+	}
+
+	// Save the thumbnail to the global map by creating new thumbnail struct with image data and media type
+	tn := thumbnail{
+		data:      thumbnailData,
+		mediaType: contentType,
+	}
+	// add thumbnail to global map `videoThumbnails` using video's ID as the key
+	videoThumbnails[videoID] = tn
+
+	// Update database so that the existing video record has a new thumbnail URL by using
+	// `cfg.db.UpdateVideo` function. The thumbnail URL should have this format:
+	// `http://localhost:<port>/api/thumbnails/{videoID}` this works since the
+	// `/api/thumbnails/{videoID}` enpoint serves thumbnails from the global map
+	thumbnailURL := fmt.Sprintf("http://localhost:%d/api/thumbnails/%s", cfg.port, videoID)
+
+	// Respond with updated JSON of the video's metadata using `respondWithJSON` function
+	// and pass it the updated `database.Video` struct to marshal.
+	video.ThumbnailURL = &thumbnailURL
+    if video.ThumbnailURL == nil || *video.ThumbnailURL != thumbnailURL {
+        respondWithError(w, http.StatusInternalServerError, "Thumbnail URL not set", err)
+        return
+    }
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
